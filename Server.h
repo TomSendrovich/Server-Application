90bba12a8e34aaccee271532e347a30c840e4c1a
Guy Shoham <guyshoham28@gmail.com>
2020-01-08 14:29:22 +0200

    auto indent

--- Server.h
+++ Server.h
@@ -8,12 +8,12 @@
 #include "ClientHandler.h"
 
 namespace server_side {
-    class Server {
-        //open the server and listening. wait for clients.
-        virtual void open(int port, ClientHandler clientHandler) = 0;
+class Server {
+  //open the server and listening. wait for clients.
+  virtual void open(int port, ClientHandler clientHandler) = 0;
 
-        //close the server.
-        virtual void stop() = 0;
-    };
+  //close the server.
+  virtual void stop() = 0;
+};
 }
 #endif //MILSTONE2_SERVER_H
