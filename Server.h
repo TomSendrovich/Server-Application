20792394ea9ef7acaff5c1a3cf622c453f1bc404
tom5012 <tsanderovich@gmail.com>
2020-01-08 14:26:11 +0200

    server_side namespace, inside there is Server interface.

---
+++ Server.h
@@ -0,0 +1,19 @@
+//
+// Created by tom5012 on 08/01/2020.
+//
+
+#ifndef MILSTONE2_SERVER_H
+#define MILSTONE2_SERVER_H
+
+#include "ClientHandler.h"
+
+namespace server_side {
+    class Server {
+        //open the server and listening. wait for clients.
+        virtual void open(int port, ClientHandler clientHandler) = 0;
+
+        //close the server.
+        virtual void stop() = 0;
+    };
+}
+#endif //MILSTONE2_SERVER_H
