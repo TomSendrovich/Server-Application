6b779c50ee7802f7cb5760219da0a17dd4c3dc1a
Guy Shoham <guyshoham28@gmail.com>
2020-01-11 12:00:30 +0200

    implement StringReverser::solve

--- StringReverser.cpp
+++ StringReverser.cpp
@@ -2,9 +2,17 @@
 // Created by guy on 10/01/2020.
 //
 
+#include <algorithm>
 #include "StringReverser.h"
-//todo: implement method
 Solution* StringReverser::solve(Problem& problem) {
-  Solution* solution = new Solution(problem.getProblem());
+
+  string str = problem.getProblem();
+  int length = str.length();
+
+  // Swap character starting from two corners
+  for (int i = 0; i < length / 2; i++)
+    swap(str[i], str[length - i - 1]);
+
+  Solution* solution = new Solution(str);
   return solution;
 }
