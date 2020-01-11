af0e80af309a5272f693f0677a0beea025ddfd0e
Guy Shoham <guyshoham28@gmail.com>
2020-01-11 13:42:04 +0200

    namespace boot

--- main.cpp
+++ main.cpp
@@ -2,26 +2,36 @@
 #include "CacheManager.h"
 #include "FileCacheManager.h"
 
-int main() {
-  int size = 5;
-  const char* problem = "3";
-  string solution;
-  FileCacheManager fileCacheManager(size);
-  bool isExist = fileCacheManager.isSolutionExist(problem);
-  if (isExist) {
-    std::cout << "true" << endl;
-    solution = fileCacheManager.getSolution(problem);
-    std::cout << solution << endl;
-  } else {
-    std::cout << "false" << endl;
+namespace boot {
+class Main {
+ public:
+  static int main() {
+    int size = 5;
+    const char* problem = "3";
+    string solution;
+    FileCacheManager fileCacheManager(size);
+    bool isExist = fileCacheManager.isSolutionExist(problem);
+    if (isExist) {
+      cout << "true" << endl;
+      solution = fileCacheManager.getSolution(problem);
+      cout << solution << endl;
+    } else {
+      cout << "false" << endl;
+    }
+
+    fileCacheManager.saveSolution("1", "tom sendrovich");
+    fileCacheManager.saveSolution("2", "guy shoham");
+    fileCacheManager.saveSolution("3", "ofir cohen");
+    fileCacheManager.saveSolution("4", "hila bartov");
+    fileCacheManager.saveSolution("5", "tzivka the king");
+    fileCacheManager.saveSolution("6", "i dont mind");
+    return 1;
   }
+};
+}
+using namespace boot;
 
-  fileCacheManager.saveSolution("1", "tom sendrovich");
-  fileCacheManager.saveSolution("2", "guy shoham");
-  fileCacheManager.saveSolution("3", "ofir cohen");
-  fileCacheManager.saveSolution("4", "hila bartov");
-  fileCacheManager.saveSolution("5", "tzivka the king");
-  fileCacheManager.saveSolution("6", "i dont mind");
-
-  return 1;
+int main() {
+  Main* main = new Main();
+  return main->main();
 }
