555a431f5310cf58f21ad15e2c38fd50639303bb
Guy Shoham <guyshoham28@gmail.com>
2020-01-13 18:07:56 +0200

    successfully reading from python server

--- MyTestClientHandler.h
+++ MyTestClientHandler.h
@@ -9,6 +9,7 @@
 #include <cstring>
 #include <sys/socket.h>
 #include <sstream>
+#include <thread>
 #include "ClientHandler.h"
 #include "CacheManager.h"
 #include "Solver.h"
@@ -18,58 +19,62 @@
 template<class P, class S>
 class MyTestClientHandler : public ClientHandler {
 
-    Solver<P, S> *_solver;
-    CacheManager<P, S> *_cm;
+  Solver<P, S>* _solver;
+  CacheManager<P, S>* _cm;
+  bool isEnd;
 
-    bool sendMsg(int out, const char* msg) {
-         int is_sent = send(out, msg, strlen(msg), 0);
-         return is_sent != -1;
-        return 2;
-    }
+  bool sendMsg(int out, const char* msg) {
+    int is_sent = send(out, msg, strlen(msg), 0);
+    return is_sent != -1;
+    return 2;
+  }
 
-public:
-    MyTestClientHandler(Solver<P, S> *solver, CacheManager<P, S> *cm) {
-        _solver = solver;
-        _cm = cm;
-    }
+ public:
+  MyTestClientHandler(Solver<P, S>* solver, CacheManager<P, S>* cm) {
+    _solver = solver;
+    _cm = cm;
+  }
 
-    ~MyTestClientHandler() override {
-        delete _solver;
-        delete _cm;
-    }
+  ~MyTestClientHandler() override {
+    delete _solver;
+    delete _cm;
+  }
 
-    void handleClient(int in) override {
-        while (true) {
-            char buffer[1024] = {0};
-            int valRead = read(in, buffer, 1024);
+  void handleClient(int in) override {
+    isEnd = false;
+    while (!isEnd) {
+      char buffer[1024] = {0};
+      int valRead = read(in, buffer, 1024);
 
-            if (valRead != 0 && valRead != -1) {
-                if (strcmp(buffer, "end") != 0) {
-                    string problem = buffer;
+      if (valRead != 0 && valRead != -1) {
+        if (strcmp(buffer, "end") != 0) {
+          string problem = buffer;
 
-                    if (_cm->isSolutionExist(problem)) {
-                        //solutionExist exist in cm, we return in to the client
-                        string solutionExist = _cm->getSolution(problem);
-                        if (!sendMsg(in, solutionExist.c_str())) {
-                            cout << "Error sending message: " << solutionExist << endl;
-                        }
-                    } else {
-                        //solutionNotExist dont exist in cm, we solve the problem and save it at cm
-                        string solutionNotExist = _solver->solve(problem);
-                        _cm->saveSolution(problem, solutionNotExist);
-                        if (!sendMsg(in, solutionNotExist.c_str())) {
-                            cout << "Error sending message: " << solutionNotExist << endl;
-                        }
-                    }
-                } else {
-                    //client wants to end communication
-                    break;
-                }
-            } else {
-                throw "ERROR: message from client did not receive";
+          if (_cm->isSolutionExist(problem)) {
+            //solutionExist exist in cm, we return in to the client
+            string solutionExist = _cm->getSolution(problem);
+            if (!sendMsg(in, solutionExist.c_str())) {
+              cout << "Error sending message: " << solutionExist << endl;
             }
+          } else {
+            //solutionNotExist dont exist in cm, we solve the problem and save it at cm
+            string solutionNotExist = _solver->solve(problem);
+            _cm->saveSolution(problem, solutionNotExist);
+            if (!sendMsg(in, solutionNotExist.c_str())) {
+              cout << "Error sending message: " << solutionNotExist << endl;
+            }
+          }
+        } else {
+          //client wants to end communication
+          cout << "end line" << endl;
+          isEnd = true;
         }
-    }
-};
+      } else {
+        throw "ERROR: message from client did not receive";
+      }
+    }//end of while loop
+  }
+
+};//end of class
 
 #endif //MILSTONE2__MYTESTCLIENTHANDLER_H_
