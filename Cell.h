6453bd0a6bb8edf2f31351629c28f9e5d561104a
Guy Shoham <guyshoham28@gmail.com>
2020-01-16 14:17:07 +0200

    starting implement BestFS

---
+++ Cell.h
@@ -0,0 +1,12 @@
+//
+// Created by guy on 16/01/2020.
+//
+
+#ifndef MILSTONE2__CELL_H_
+#define MILSTONE2__CELL_H_
+
+class Cell {
+
+};
+
+#endif //MILSTONE2__CELL_H_
---
+++ Position.h
@@ -0,0 +1,12 @@
+//
+// Created by guy on 16/01/2020.
+//
+
+#ifndef MILSTONE2__POSITION_H_
+#define MILSTONE2__POSITION_H_
+
+class Position {
+
+};
+
+#endif //MILSTONE2__POSITION_H_
