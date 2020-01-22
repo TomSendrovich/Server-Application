751339f0ecc4e70e3de934f1456913e161833017
Guy Shoham <guyshoham28@gmail.com>
2020-01-22 21:32:42 +0200

    DepthFirstSearch compiles

--- DepthFirstSearch.h
+++ DepthFirstSearch.h
@@ -11,14 +11,17 @@
 template<typename T>
 class DepthFirstSearch : public Searcher<T> {
  public:
-  DepthFirstSearch() {}
-  list<State<T>*> search(Searchable<T>* problem) override {
-    addToOpenQueue(problem->getInitialState());
+  DepthFirstSearch() {
+    Searcher<T>::evaluatedNodes = 0;
+    Searcher<T>::openQueue = new priority_queue<State<T>*>();
+  }
+  list<State<T>*>* search(Searchable<T>* problem) override {
+    Searcher<T>::addToOpenQueue(problem->getInitialState());
 
     while (Searcher<T>::openQueueSize() > 0) {
       State<T>* node = Searcher<T>::popOpenQueue();
 
-      if (node->equals(problem->getInitialState())) {
+      if (node == problem->getInitialState()) {
         node->setPathCost(node->getCost());
       }
 
@@ -26,36 +29,36 @@ class DepthFirstSearch : public Searcher<T> {
       node->setIsDiscovered(true);
 
       if (problem->isGoalState(node)) {
-        list<State<T>*> retVal = backTrace(problem->getInitialState(), node);
+        list<State<T>*>* retVal = Searcher<T>::backTrace(problem->getInitialState(), node);
         return retVal;
       }
 
       list<State<T>*> successors = problem->getAllPossibleStates(node);
       for (State<T>* s: successors) {
         if (!s->isDiscovered()) {
-          addToOpenQueue(s);
-          popOpenQueue(s);
-          DFS(problem, s);
+          Searcher<T>::addToOpenQueue(s);
+          Searcher<T>::popOpenQueue();
+          return DFS(problem, s);
         }
       }
     }
     return nullptr;
   }
 
-  void DFS(Searchable<T>* problem, State<T>* node) {
+  list<State<T>*>* DFS(Searchable<T>* problem, State<T>* node) {
     node->setIsDiscovered(true);
-    node->setPathCost()(node->getParent()->getPathCost() + node->getCost());
+    node->setPathCost(node->getParent()->getPathCost() + node->getCost());
 
     if (problem->isGoalState(node)) {
-      list<State<T>*> retVal = backTrace(problem->getInitialState(), node);
+      list<State<T>*>* retVal = Searcher<T>::backTrace(problem->getInitialState(), node);
       return retVal;
     }
 
     list<State<T>*> successors = problem->getAllPossibleStates(node);
     for (State<T>* s: successors) {
       if (!s->isDiscovered()) {
-        addToOpenQueue(s);
-        DFS(problem, s);
+        Searcher<T>::addToOpenQueue(s);
+        return DFS(problem, s);
       }
     }
   }
